72e33488ec10df02bf02f48677819904b3077637
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-07-30 16:33:10 +0530

    create like videos controller

---
+++ controllers/liked.videos.controller.js
@@ -0,0 +1,48 @@
+const {LikedVideos} = require("../models/likedVideo.model");
+
+exports.get_user_liked_videos = async(req,res)=>{
+    try {
+      const {userId} = req.params;
+      // .populate("historyVideos _id")
+        const getUserVideos = await LikedVideos.findById(userId)  
+        res.json({message:"user videos are",getUserVideos})
+    } catch (error) {
+        res.status(500).json({errorMessage:"error while getting user videos"})
+    }
+  }
+
+  exports.add_video_to_user_liked =  async(req,res)=>{
+    try {
+      const {userId} = req.params;
+      const userLikedVideos = await LikedVideos.findById(userId);
+      if(userLikedVideos === null){
+        const AddVideoToLiked = new LikedVideos({
+          _id : userId,
+          likedVideos:[req.body]
+        })
+        await AddVideoToLiked.save()
+        return res.json({success:true,message:"Video added sucessfully to liked videos"})
+      }
+      await LikedVideos.updateOne({"_id":userId},{
+        "$addToSet":{
+          "likedVideos":req.body._id
+        }
+      })
+      return res.json({success:true,message:"Video added sucessfully to liked videos"})
+      
+    } catch (error) {
+      res.json({ errorMessage: "video not added to liked videos" })
+    }
+  }
+
+  exports.delete_videos_from_user_liked = async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const deleteVideo = await LikedVideos.findById(userId)
+      await deleteVideo.likedVideos.remove(req.body._id);
+      await deleteVideo.save()
+      res.json({ sucess: true, message: "Video removed successfully from user liked videos model" })
+    }catch{
+      res.status(500).json({errorMessage:"Video not removed"})
+    }
+  }
