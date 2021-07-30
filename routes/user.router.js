325a9dcacd18a80895d595825351289cbacfd61a
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-07-30 18:28:07 +0530

    add :playlist management

--- routes/user.router.js
+++ routes/user.router.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const UserController = require("../controllers/user.controller");
 const HistoryVideosController = require("../controllers/history.videos.controller");
 const LikedVideosController = require("../controllers/liked.videos.controller");
+const PlaylistController = require("../controllers/playlist.controller");
+const PlaylistVideoController = require("../controllers/playlist.video.controller");
+
 
 router.route("/")
 .get(UserController.get_all_users)
@@ -20,5 +23,14 @@ router.route("/:userId/liked/video")
 .get(LikedVideosController.get_user_liked_videos)
 .post(LikedVideosController.add_video_to_user_liked)
 .delete(LikedVideosController.delete_videos_from_user_liked)
+
+router.route("/:userId/playlists")
+.get(PlaylistController.get_user_playlists)
+.post(PlaylistController.add_playlist_to_user)
+.delete(PlaylistController.delete_playlist_of_user)
+
+router.route("/:userId/playlists/video")
+.post(PlaylistVideoController.add_video_to_user_playlist)
+.delete(PlaylistVideoController.delete_video_from_user_playlist)
 
 module.exports = router;
