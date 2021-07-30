40745a62067dcdb3cfca7de67a2ac95e10f01701
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-07-30 18:28:35 +0530

    add playlist video controllers

---
+++ controllers/playlist.video.controller.js
@@ -0,0 +1,40 @@
+const {PlayList} = require("../models/playlist.model");
+
+exports.add_video_to_user_playlist = async(req, res) => {
+    try {
+        const {userId} = req.params;
+        const {name} = req.body;
+        const {videoId} = req.body;
+        await PlayList.updateOne({
+            "_id": userId,
+            "playlists.name": name
+        }, {
+            "$addToSet": {
+                "playlists.$.video": videoId
+            }
+        })
+        res.json({message: "Video added to playlist"})
+    } catch (error) {
+        res.status(500).json({errorMessage: "video not added to playlist"})
+    }
+}
+
+exports.delete_video_from_user_playlist = async(req, res) => {
+    try {
+        const {userId} = req.params;
+        const {name} = req.body;
+        const {videoId} = req.body;
+        await PlayList.updateOne({
+            "_id": userId,
+            "playlists.name": name
+        }, {
+            "$pull": {
+                "playlists.$.video": videoId
+            }
+        });
+        res.json({success: true, message: "video deleted from playlist.."})
+    } catch (error) {
+        res.status(500).json({errorMessage: "video not deleted from playlist"})
+    }
+    
+}
