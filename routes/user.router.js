50378597c49a25ceb08602c91d6c4a3cc5060b18
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-07-30 16:32:39 +0530

    add : history and liked video routes

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require("../controllers/user.controller");
-const { User } = require('../models/user.model');
-
+const HistoryVideosController = require("../controllers/history.videos.controller");
+const LikedVideosController = require("../controllers/liked.videos.controller");
 
 router.route("/")
 .get(UserController.get_all_users)
 .post(UserController.add_user)
 
 router.route("/login")
-  .post(UserController.user_login)
+.post(UserController.user_login)
+
+router.route("/:userId/history/video")
+.get(HistoryVideosController.get_user_history_videos)
+.post(HistoryVideosController.add_video_to_user_history)
+.delete(HistoryVideosController.delete_videos_from_user_history)
+
+router.route("/:userId/liked/video")
+.get(LikedVideosController.get_user_liked_videos)
+.post(LikedVideosController.add_video_to_user_liked)
+.delete(LikedVideosController.delete_videos_from_user_liked)
+
 module.exports = router;
