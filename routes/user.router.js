b44d212bdaa1db03951b28b0abf138a08befc1ce
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-05 23:52:25 +0530

    fix : change routes and make delete to post

--- routes/user.router.js
+++ routes/user.router.js
@@ -19,20 +19,27 @@ router.use(userLogger)
 router.route("/:userId/history/video")
 .get(HistoryVideosController.get_user_history_videos)
 .post(HistoryVideosController.add_video_to_user_history)
-.delete(HistoryVideosController.delete_videos_from_user_history)
+
+router.route("/:userId/history/video/delete")
+.post(HistoryVideosController.delete_videos_from_user_history)
 
 router.route("/:userId/liked/video")
 .get(LikedVideosController.get_user_liked_videos)
 .post(LikedVideosController.add_video_to_user_liked)
-.delete(LikedVideosController.delete_videos_from_user_liked)
+
+router.route("/:userId/liked/video/delete")
+.post(LikedVideosController.delete_videos_from_user_liked)
 
 router.route("/:userId/playlists")
 .get(PlaylistController.get_user_playlists)
 .post(PlaylistController.add_playlist_to_user)
-.delete(PlaylistController.delete_playlist_of_user)
+router.route("/:userId/playlists/delete")
+.post(PlaylistController.delete_playlist_of_user)
 
 router.route("/:userId/playlists/video")
 .post(PlaylistVideoController.add_video_to_user_playlist)
-.delete(PlaylistVideoController.delete_video_from_user_playlist)
+
+router.route("/:userId/playlists/video/delete")
+.post(PlaylistVideoController.delete_video_from_user_playlist)
 
 module.exports = router;
